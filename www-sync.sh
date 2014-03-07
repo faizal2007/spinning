6ec2e8009389dfcd66f6888fd81520f5de42c51f
Mohd Faizal <freakie2007@gmail.com>
2014-03-08 05:37:18 +0800

    Adding base script

---
+++ www-sync.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#
+# rsync option
+# Refer man rsync
+#
+
+OPTION="-azvv --progress --delete -e ssh"
+
+#
+# ssh authentication 
+#
+USER="user"
+HOST="10.2.0.101"
+
+#
+# Server Setting
+#
+
+## Production Server
+SOURCE="/var/www/"
+
+## Local Server
+DESTINATION="/home/local/web/"
+
+## Exclude File
+## Split file by <space>
+## Comment EXCLUDE_FILE to sync all
+EXCLUDE_FILE[0]="configuration.php"
+#EXCLUDE_FILE[1]="smnd/index.php"
+
+#
+# Log
+#
+LOGS="/var/log/www-sync/"
+## date format ##
+NOW=$(date +"%F")
+NOWT=$(date +"%T")
+
+
+#
+# @method logging script activity 
+#
+
+#function logging() {
+#    echo $NOWT
+#}
+
+#
+# @method to sync directory and file
+#
+
+function st_sync() {
+    if [ ${#EXCLUDE_FILE[@]} -eq 0 ]; then
+        EXCLUDE=""
+    else
+        for FILE in "${EXCLUDE_FILE[@]}"
+        do
+            :
+            EXCLUDE=$EXCLUDE"--exclude $FILE "
+        done 
+    fi
+
+    rsync $OPTION $EXCLUDE $USER@$HOST:$SOURCE $DESTINATION
+}
+
+st_sync
+#logging
