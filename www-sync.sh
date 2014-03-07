18e96c4b6cee5caad1944e129422f4415a7dc11b
Mohd Faizal <freakie2007@gmail.com>
2014-03-08 06:10:32 +0800

    Add multiple source sync

--- www-sync.sh
+++ www-sync.sh
@@ -18,10 +18,12 @@ HOST="10.2.0.101"
 #
 
 ## Production Server
-SOURCE="/var/www/"
+SOURCE[0]="/var/www/"
+SOURCE[1]="/var/storage/"
 
 ## Local Server
-DESTINATION="/home/local/web/"
+DESTINATION[0]="/home/local/web/"
+DESTINATION[1]="/home/local/lib/"
 
 ## Exclude File
 ## Split file by <space>
@@ -67,7 +69,14 @@ function st_sync() {
         done 
     fi
 
-    rsync $OPTION $EXCLUDE $USER@$HOST:$SOURCE $DESTINATION
+    #rsync $OPTION $EXCLUDE $USER@$HOST:$SOURCE $DESTINATION
+    i=0
+    for SOURCE in "${SOURCE[@]}"
+    do
+        :
+        rsync $OPTION $EXCLUDE $USER@$HOST:$SOURCE ${DESTINATION[$i]}
+        i=$(expr $i + 1)
+    done
 }
 
 st_sync
